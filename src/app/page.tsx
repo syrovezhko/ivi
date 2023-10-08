ac92af0245bd2c00b625cf31f99993ffee60fab2
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-10-08 20:03:34 +0300

    feat: mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
-import styles from './page.module.css'
+const listArr = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatem fugiat reprehenderit quae, dicta, consectetur eos qui quod sed esse perferendis doloribus iure quos inventore. Accusantium non tenetur aspernatur impedit?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatem fugiat reprehenderit quae, dicta, consectetur eos qui quod sed esse perferendis doloribus iure quos inventore. Accusantium non tenetur aspernatur impedit?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatem fugiat reprehenderit quae, dicta, consectetur eos qui quod sed esse perferendis doloribus iure quos inventore. Accusantium non tenetur aspernatur impedit?"
+];
+
+const promo = {
+  title:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatem fugiat reprehenderit quae, dicta.",
+  breakpoints: ["360p", "720p", "HD", "FullHD", "4K"],
+};
 
 export default function Home() {
   return (
